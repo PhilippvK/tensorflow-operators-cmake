277947c59a35e0a651c7d48fcbd96ed3edfef9e3
Philipp v. K <philipp.van-kempen@tum.de>
2021-04-21 17:10:47 +0200

    add tflite ops complex,imag,zero_out

---
+++ tflite/complex/src/custom_implementations.cpp
@@ -0,0 +1,44 @@
+#include "tensorflow/lite/c/builtin_op_data.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+
+namespace tflite {
+namespace ops {
+namespace micro {
+namespace complex {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *)
+{
+    return kTfLiteOk;
+}
+}  // namespace reduce_max
+TfLiteRegistration *Register_Complex(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      complex::Eval,
+  };
+  return &res;
+}
+namespace imag {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *)
+{
+    return kTfLiteOk;
+}
+}  // namespace reduce_max
+TfLiteRegistration *Register_Imag(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      imag::Eval,
+  };
+  return &res;
+}
+}  // namespace micro
+}  // namespace ops
+}  // namespace tflite
+
+void register_customs(tflite::AllOpsResolver *res) {
+  res->AddCustom("Complex", tflite::ops::micro::Register_Complex());
+  res->AddCustom("Imag", tflite::ops::micro::Register_Imag());
+}
